8d954beea08829cdf0a138506a29a53c0302b8d1
svp19 <170010025@iitdh.ac.in>
2020-01-12 11:22:38 +0530

    Tested

--- lab1/uniformed_search.cpp
+++ lab1/uniformed_search.cpp
@@ -8,11 +8,13 @@ pii nil = {-1, -1};
 struct node{
     char value;
     bool visited;
+    bool explored;
     pii parent;
     int depth;
     node(){
         parent = zero;
         depth = 0;
+        explored = false;
         visited = false;
         value = -1;
     }
@@ -23,7 +25,8 @@ class Maze{
     vector<vector<node>> G; //Graph
     int M, N; //Dimensions
     int algo; //0 -> bfs, 1 -> dfs, 2 -> dfid  
-
+    int nodes_visited; // no. nodes visited in i'th iteration of dfid
+    int max_bound; // dfid bound on i'th iteration
 
     node* get_node(pii pos){
         return &(G[pos.first][pos.second]);
@@ -36,24 +39,28 @@ class Maze{
                 G[i][j].parent = zero;
                 G[i][j].depth = 0;
                 G[i][j].visited = false;
+                G[i][j].explored = false;
             }
     }
 
 
-    vector<pii> moveGen(pii pos){
+    vector<pii> moveGen(pii pos, bool rev=false){
         vector<pii> neighbours; 
         int x, y;
-        int rot_x[] = {-1, 0, 1, 0};
-        int rot_y[] = {0, -1, 0, 1};
+        // Rotation -> DOWN > UP > RIGHT > LEFT
+        int rot_x[] = {1, -1, 0, 0};
+        int rot_y[] = {0, 0, 1, -1};
         for(int i=0; i<4; ++i){
             x = pos.first + rot_x[i]; 
             y = pos.second + rot_y[i];
-
             if(x >= 0 && x < M && y >= 0 && y < N){// if valid x, y co-ordinates
-                if(G[x][y].value == ' ' || G[x][y].value == '*')
+                if(G[x][y].value == ' ' || G[x][y].value == '*'){
                     neighbours.push_back(make_pair(x, y));
+                }
             }
         }
+        if(rev)
+            std::reverse(neighbours.begin(), neighbours.end());
         return neighbours;
     }
 
@@ -70,7 +77,7 @@ class Maze{
         queue<pii> Q;
 
         // Push Source
-        Q.push(make_pair(0,0));    
+        Q.push(zero);    
 
         while(!Q.empty()){
 
@@ -79,7 +86,7 @@ class Maze{
             Q.pop();
 
             // Mark source visited
-            G[source.first][source.second].visited = true;
+            get_node(source)->visited = true;
 
             // Goal test
             if(goalTest(source))
@@ -88,7 +95,7 @@ class Maze{
             // Check neighbours
             vector<pii> neighbours = moveGen(source);
             for(pii n: neighbours){
-                if(!G[n.first][n.second].visited){
+                if(!get_node(n)->visited){
                     G[n.first][n.second].parent = source;
                     Q.push(n);
                 }
@@ -97,21 +104,24 @@ class Maze{
         return {-1, -1};        
     }
 
-    pii dfs(int bound = -1){
+
+    pii dfs(){
 
         stack<pii> S;
 
         // Push Source
-        S.push(make_pair(0,0));    
+        S.push(zero);    
 
         while(!S.empty()){
 
             // Pop source
             pii source = S.top();
+            // cout << "Exploring " << source.first << ", " << source.second << "\n";
             S.pop();
 
             // Mark source visited
             get_node(source)->visited = true;
+            get_node(source)->explored = true;
 
             // Mark depth of node using parent
             if(source != zero)
@@ -121,14 +131,12 @@ class Maze{
             if(goalTest(source))
                 return source;
 
-            if(get_node(source)->depth == bound)
-                continue;
-
             // Check neighbours
-            vector<pii> neighbours = moveGen(source);
+            vector<pii> neighbours = moveGen(source, true);
             for(pii n: neighbours){
-                if(!G[n.first][n.second].visited){
-                    G[n.first][n.second].parent = source;
+                if(!G[n.first][n.second].explored){
+                    get_node(n)->parent = source;
+                    get_node(n)->explored = true;
                     S.push(n);
                 }
             }
@@ -136,17 +144,42 @@ class Maze{
         return {-1, -1};        
     }
 
+
+    pii dls(pii source, int bound){
+        nodes_visited ++;
+        node* src = get_node(source);
+        src->visited = true;
+        
+        if(goalTest(source))
+            return source;
+
+        if(bound > 0){
+            for(pii n: moveGen(source)){
+                node* nb = get_node(n); 
+                if(nb->visited && src->depth + 1 >= nb->depth)
+                    continue;
+                nb->depth = src->depth + 1;
+                nb->parent = source;
+
+                pii goal = dls(n, bound-1);
+                if(goal != nil)
+                    return goal;                
+            }
+        }
+        return nil;
+    }
+
     pii dfid(){
         int bound = 0;
+        nodes_visited = 0;
         while(true){
             reset_nodes();
-            pii goal = dfs(bound);
+            max_bound = bound;
+            pii goal = dls(zero, bound);
             if(goalTest(goal)){
                 return goal;
             }
             bound++;
-            if(bound>=25)
-                break;
         } 
     }
 
@@ -216,10 +249,14 @@ public:
 
         // number of states
         int num_states = 0;
-        for(int i=0; i<M; ++i)
-            for(int j=0; j<N; ++j)
-                if(G[i][j].visited)
-                    num_states ++;
+        if(algo != 2){
+            for(int i=0; i<M; ++i)
+                for(int j=0; j<N; ++j)
+                    if(G[i][j].visited)
+                        num_states ++;
+        } else {
+            num_states = nodes_visited - 1;
+        }
         cout << num_states << "\n" ;
 
         // length of path found
