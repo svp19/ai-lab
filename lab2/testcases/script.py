1ea85e7d922675abdc55c78033998b7c9a53b2de
deepakhr1999 <deepakhr1999@gmail.com>
2020-01-19 07:56:57 +0530

    see what we can submit

---
+++ lab2/testcases/script.py
@@ -0,0 +1,30 @@
+"""
+For generation random matrices, go to https://onlinemathtools.com/generate-random-matrix
+
+Download them into the directory as input1.txt, input2.txt etc
+
+And to get the solutions, run
+$python3 script.py > output.txt
+
+"""
+import os
+import requests
+i = 1
+files = os.listdir()
+while f"input{i}.txt" in files:
+    with open(f"input{i}.txt", "r") as file:
+        text = []
+        for line in file:
+            line = "-".join(line.split())
+            text.append(line)
+        text = "--".join(text)
+        text = f"http://www.hungarianalgorithm.com/solve.php?c={text}&random=1"
+        text = text[:46]+text[47:]
+        text = requests.get(text).text
+        index = text.index("optimal value equals ")+21
+        text = text[index:]
+        index = text.index(".")
+        text = text[:index]
+        print(text)
+    i += 1
+        
