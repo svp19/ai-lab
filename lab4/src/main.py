0f683e495267124d568a8334141550be66470d31
deepakhr1999 <deepakhr1999@gmail.com>
2020-02-22 06:35:31 +0530

    A* algorithm

---
+++ lab4/src/main.py
@@ -0,0 +1,87 @@
+import sys
+from queue import PriorityQueue
+from utils import Point
+
+class Astar(object):
+    def __init__(self):
+        self.N = 0
+        self.V = []
+        self.E = []
+
+    def goalTest(self, u):
+        return u == self.N - 1
+    
+    def moveGen(self, u):
+        return zip(range(self.N), self.E[u])
+
+    def f(self, u):
+        return self.g(u) + self.h(u)
+
+    def g(self, u):
+        return self.V[u].value
+
+    def h(self, u):
+        return self.V[u].distance(self.V[-1])
+
+    def takeInput(self, filename):
+        with open(filename, "r") as file:
+            self.N = int( next(file).rstrip() )
+            readLine = lambda x: list(map(int, x.rstrip().split()))
+            self.V = [ Point( *readLine(next(file)) ) for i in range(self.N) ]
+            self.E = [ readLine(next(file)) for i in range(self.N) ]
+
+    def reconstructPath(self):
+        v = self.N - 1
+        path = []
+        while v is not None:
+            path.append(v)
+            v = self.V[v].parent
+        path.reverse()
+        cost = sum(self.E[path[i-1]][path[i]] for i in range(1, len(path)))
+        return cost, path
+
+    def getShortestPath(self):
+        """
+            calculate the shortest path from vertex 0 and N-1
+            returns cost, path
+
+            g(u): path length from 0 to u
+            h(u): euclidean distance from u to goal
+            f(u) = g(u) + h(u), used as p in priority queue
+
+        """
+        Q = PriorityQueue() # implemented with lazy update
+        self.V[0].value = 0
+        Q.put( (self.f(0), 0) )
+
+        
+        closed = set([0])
+        
+        while not Q.empty():
+            f, u = Q.get()
+            
+            if self.goalTest(u):
+                return self.reconstructPath()
+            
+            closed.add(u)
+
+            for v, w in self.moveGen(u):
+                if w != 0 and v not in closed:
+                    # add to queue only if this reduces the path length
+                    newValue = self.g(u) + w
+                    
+                    if newValue < self.g(v):
+                        self.V[v].value = newValue
+                        self.V[v].parent = u
+                        Q.put( (self.f(v), v) )
+                    
+
+    def testPrint(self, filename):
+        self.takeInput(filename)
+        cost, path = self.getShortestPath()
+        print(cost)
+        print(*path, sep=" ")
+
+
+A = Astar()
+A.testPrint(sys.argv[1])
